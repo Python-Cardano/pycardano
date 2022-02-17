eeadf6f3767dc3d6e0524f68e4572d193e77203f
Jerry <jerry@staking247.com>
2022-02-17 00:54:33 -0800

    A fullstack example (flask + PyCardano + BlockFrost + React + Nami wallet)

---
+++ examples/full_stack/README.md
@@ -0,0 +1,50 @@
+
+
+This is a full stack demo on transaction creation with PyCardano and transaction signing with
+[Nami](https://namiwallet.io/) wallet. 
+
+The backend is hosted by a simple [flask](https://flask.palletsprojects.com/en/2.0.x/) application. 
+The frontend is rendered using [React](https://reactjs.org/). 
+
+This webapp allows users to send ADA to multiple addresses, which is currently not possible with only Nami wallet.  
+
+####Note
+This is a simple demo built in a few hours. It is not fully tested and some designs could be inefficient.
+
+### Steps
+
+1. Install dependencies
+
+```shell
+$ poetry install
+```
+
+
+2. Start flask app
+
+```shell
+$ export FLASK_APP=server
+$ flask run
+```
+
+3. Open a browser that has Nami wallet extension installed, go to 
+
+```http://127.0.0.1:5000/```
+
+
+4. Connect the wallet by clicking "Connect wallet" button. 
+
+5. Paste multiple Cardano addresses and specify the amount of ADA to send for each one.  
+
+6. When you are done with adding addresses, click "Submit Tx" button, and Nami will prompt a signing window.
+
+7. Sign the transaction and you are all set. You should be able to see the transaction soon in the blockchain explorer.
+
+
+### Demo
+
+![Alt Text](demo.gif)
+
+### Sequence Diagram
+
+![Alt Text](sequence_diagram.svg)
---
+++ examples/full_stack/server.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request
+
+from pycardano import (
+    Address,
+    BlockFrostChainContext,
+    Network,
+    Transaction,
+    TransactionBuilder,
+    TransactionOutput,
+    TransactionWitnessSet,
+)
+
+app = Flask(__name__)
+
+# Use BlockFrostChainContext for simplicity. You can also implement your own chain context.
+chain_context = BlockFrostChainContext("your_project_id", network=Network.TESTNET)
+
+
+def build_transaction(data):
+    input_addresses = [
+        Address.from_primitive(bytes.fromhex(sender)) for sender in data["senders"]
+    ]
+    change_address = Address.from_primitive(bytes.fromhex(data["change_address"]))
+    transaction_outputs = [
+        TransactionOutput.from_primitive([address, int(amount) * 1000000])
+        for address, amount in data["recipients"]
+    ]
+
+    print(f"Input addresses: {input_addresses}")
+    print(f"Transaction outputs: {transaction_outputs}")
+    print(f"Change address: {change_address}")
+
+    builder = TransactionBuilder(chain_context)
+    for input_address in input_addresses:
+        builder.add_input_address(input_address)
+    for transaction_output in transaction_outputs:
+        builder.add_output(transaction_output)
+
+    tx_body = builder.build(change_address=change_address)
+    return Transaction(tx_body, TransactionWitnessSet())
+
+
+def compose_tx_and_witness(data):
+    tx = Transaction.from_cbor(data["tx"])
+    witness = TransactionWitnessSet.from_cbor(data["witness"])
+    tx.transaction_witness_set = witness
+    return tx
+
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+@app.route("/build_tx", methods=["POST"])
+def build_tx():
+    tx = build_transaction(request.json)
+    cbor_hex = tx.to_cbor()
+    print(cbor_hex)
+    return {"tx": cbor_hex}
+
+
+@app.route("/submit_tx", methods=["POST"])
+def submit_tx():
+    tx = compose_tx_and_witness(request.json)
+    tx_id = tx.transaction_body.hash().hex()
+    print(f"Transaction: \n {tx}")
+    print(f"Transaction cbor: {tx.to_cbor()}")
+    print(f"Transaction ID: {tx_id}")
+    chain_context.submit_tx(tx.to_cbor())
+    return {"tx_id": tx_id}
