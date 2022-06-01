9330c4b88dfbda4da3c8346e5ce9791df5e4128d
Jarred Green <astrojarred@gmail.com>
2022-06-01 20:57:29 +0200

    Revert key module

--- test/pycardano/test_key.py
+++ test/pycardano/test_key.py
@@ -4,7 +4,6 @@ import tempfile
 from pycardano.key import (
     ExtendedSigningKey,
     ExtendedVerificationKey,
-    Message,
     PaymentKeyPair,
     PaymentSigningKey,
     PaymentVerificationKey,
@@ -98,58 +97,3 @@ def test_key_save():
         SK.save(f.name)
         sk = PaymentSigningKey.load(f.name)
         assert SK == sk
-
-
-def test_verify_message():
-
-    signed_message = "845869a3012704582060545b786d3a6f903158e35aae9b86548a99bc47d4b0a6f503ab5e78c1a9bbfc6761646472657373583900ddba3ad76313825f4f646f5aa6d323706653bda40ec1ae55582986a463e661768b92deba45b5ada4ab9e7ffd17ed3051b2e03500e0542e9aa166686173686564f452507963617264616e6f20697320636f6f6c2e58403b09cbae8d272ff94befd28cc04b152aea3c1633caffb4924a8a8c45be3ba6332a76d9f2aba833df53803286d32a5ee700990b79a0e86fab3cccdbfd37ce250f"
-
-    message = Message(signed_message=signed_message)
-
-    assert message.verify(cose_key_separate=False) is True
-    assert message.message == "Pycardano is cool."
-    assert (
-        str(message.signing_address)
-        == "addr_test1qrwm5wkhvvfcyh60v3h44fknydcxv5aa5s8vrtj4tq5cdfrrueshdzujm6aytddd5j4eullazlknq5djuq6spcz596dqjvm8nu"
-    )
-
-
-def test_verify_message_cose_key_separate():
-
-    signed_message = {
-        "signature": "845846a201276761646472657373583900ddba3ad76313825f4f646f5aa6d323706653bda40ec1ae55582986a463e661768b92deba45b5ada4ab9e7ffd17ed3051b2e03500e0542e9aa166686173686564f452507963617264616e6f20697320636f6f6c2e584040b65c973ba6e123f1e7f738205b10c709fe214a27d21b1c382e6dfa5772aaeeb6222943fd56b1dd6bfa5abfa4a4992d2abde110cbd0c8651fdfa679ba462605",
-        "key": "a401010327200621582060545b786d3a6f903158e35aae9b86548a99bc47d4b0a6f503ab5e78c1a9bbfc",
-    }
-
-    message = Message(signed_message=signed_message)
-
-    assert message.verify(cose_key_separate=True) is True
-    assert message.message == "Pycardano is cool."
-    assert (
-        str(message.signing_address)
-        == "addr_test1qrwm5wkhvvfcyh60v3h44fknydcxv5aa5s8vrtj4tq5cdfrrueshdzujm6aytddd5j4eullazlknq5djuq6spcz596dqjvm8nu"
-    )
-
-
-def test_sign_message():
-
-    message = Message(message="Pycardano is cool.")
-    signed_message = message.sign(
-        signing_key=SK, verification_key=VK, cose_key_separate=False
-    )
-    assert (
-        signed_message
-        == "84582da20127676164647265737358208be8339e9f3addfa6810d59e2f072f85e64d4c024c087e0d24f8317c6544f62fa166686173686564f452507963617264616e6f20697320636f6f6c2e584045009400ef415f5635a363e5a6cb06c08dd2e9b677cd5317463018f2a5430d9faed85e773ade91e65d7579ccdd4934aefac64e48722d50dd492931784332a30b"
-    )
-
-
-def test_sign_message_cosy_key_separate():
-
-    message = Message(message="Pycardano is cool.")
-    signed_message = message.sign(
-        signing_key=SK, verification_key=VK, cose_key_separate=True
-    )
-    assert signed_message == {
-        "signature": "845850a30127676164647265737358208be8339e9f3addfa6810d59e2f072f85e64d4c024c087e0d24f8317c6544f62f0458208be8339e9f3addfa6810d59e2f072f85e64d4c024c087e0d24f8317c6544f62fa166686173686564f452507963617264616e6f20697320636f6f6c2e5840b530b779ab8c78c445d91dd859878bdc94e7900865a920d6bc85a98b7b74c794eb4ef22a2712aa0d29840ab14a884aca32609001594bdde5368515d981b2fd0f",
-        "key": b"a40101032720062158208be8339e9f3addfa6810d59e2f072f85e64d4c024c087e0d24f8317c6544f62f",
-    }
