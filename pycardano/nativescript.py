533444a781ede881b6eeb74f8f53adc6149422ac
Jarred Green <astrojarred@gmail.com>
2022-07-07 00:31:51 +0200

    Add json_tag/field params to each native script

--- pycardano/nativescript.py
+++ pycardano/nativescript.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import List, Union
+from typing import ClassVar, List, Union
 
 from nacl.encoding import RawEncoder
 from nacl.hash import blake2b
@@ -59,21 +59,14 @@ class NativeScript(ArrayCBORSerializable):
         ScriptPubkey, ScriptAll, ScriptAny, ScriptNofK, InvalidBefore, InvalidHereAfter
     ]:
 
-        TYPES = {
-            "sig": ScriptPubkey,
-            "all": ScriptAll,
-            "any": ScriptAny,
-            "atLeast": ScriptNofK,
-            "after": InvalidBefore,
-            "before": InvalidHereAfter,
-        }
+        types = {p.json_tag: p for p in [ScriptPubkey, ScriptAll, ScriptAny, ScriptNofK, InvalidBefore, InvalidHereAfter]}
 
         if isinstance(script, dict):
             native_script = []
 
             for key, value in script.items():
                 if key == "type":
-                    native_script.insert(0, list(TYPES.keys()).index(value))
+                    native_script.insert(0, list(types.keys()).index(value))
                 elif key == "scripts":
                     native_script.append(cls.from_dict(value, top_level=False))
                 else:
@@ -85,42 +78,33 @@ class NativeScript(ArrayCBORSerializable):
         if not top_level:
             return native_script
         else:
-            return super(NativeScript, TYPES[script["type"]]).from_primitive(
+            return super(NativeScript, types[script["type"]]).from_primitive(
                 native_script[1:]
             )
 
     def to_dict(self) -> dict:
 
-        TAGS = [
-            "sig",
-            "all",
-            "any",
-            "atLeast",
-            "after",
-            "before",
-        ]
-
-        FIELDS = ["keyHash", "scripts", "scripts", "required", "slot", "slot"]
-
         script = {}
 
         for value in self.__dict__.values():
-            script["type"] = TAGS[self._TYPE]
+            script["type"] = self.json_tag
 
             if isinstance(value, list):
                 script["scripts"] = [i.to_dict() for i in value]
 
             else:
                 if isinstance(value, int):
-                    script[FIELDS[self._TYPE]] = value
+                    script[self.json_field] = value
                 else:
-                    script[FIELDS[self._TYPE]] = str(value)
+                    script[self.json_field] = str(value)
 
         return script
 
 
 @dataclass
 class ScriptPubkey(NativeScript):
+    json_tag: ClassVar[str] = "sig"
+    json_field: ClassVar[str] = "keyHash"
     _TYPE: int = field(default=0, init=False)
 
     key_hash: VerificationKeyHash
@@ -128,6 +112,8 @@ class ScriptPubkey(NativeScript):
 
 @dataclass
 class ScriptAll(NativeScript):
+    json_tag: ClassVar[str] = "all"
+    json_field: ClassVar[str] = "scripts"
     _TYPE: int = field(default=1, init=False)
 
     native_scripts: List[
@@ -144,6 +130,8 @@ class ScriptAll(NativeScript):
 
 @dataclass
 class ScriptAny(NativeScript):
+    json_tag: ClassVar[str] = "any"
+    json_field: ClassVar[str] = "scripts"
     _TYPE: int = field(default=2, init=False)
 
     native_scripts: List[
@@ -160,6 +148,8 @@ class ScriptAny(NativeScript):
 
 @dataclass
 class ScriptNofK(NativeScript):
+    json_tag: ClassVar[str] = "atLeast"
+    json_field: ClassVar[str] = "required"
     _TYPE: int = field(default=3, init=False)
 
     n: int
@@ -178,6 +168,8 @@ class ScriptNofK(NativeScript):
 
 @dataclass
 class InvalidBefore(NativeScript):
+    json_tag: ClassVar[str] = "after"
+    json_field: ClassVar[str] = "slot"
     _TYPE: int = field(default=4, init=False)
 
     before: int = None
@@ -185,6 +177,8 @@ class InvalidBefore(NativeScript):
 
 @dataclass
 class InvalidHereAfter(NativeScript):
+    json_tag: ClassVar[str] = "before"
+    json_field: ClassVar[str] = "slot"
     _TYPE: int = field(default=5, init=False)
 
     after: int = None
