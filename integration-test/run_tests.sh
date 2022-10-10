fa72a9115489378c2fc6281b58b61b4bc57eeed0
Jerry <jerrycgh@gmail.com>
2022-10-09 22:36:50 -0700

    Test

--- integration-test/run_tests.sh
+++ integration-test/run_tests.sh
@@ -7,6 +7,29 @@ ROOT=$(pwd)
 
 poetry install
 
+##########
+# Alonzo #
+##########
+
+# Cleanup containers and volumes in case there is any running
+docker-compose down --volumes --remove-orphans
+
+# Run alonzo integration tests
+./bootstrap.sh local-alonzo
+
+# Launch containers
+docker-compose up -d
+
+export PAYMENT_KEY="$ROOT"/configs/local-alonzo/shelley/utxo-keys/utxo1.skey
+export EXTENDED_PAYMENT_KEY="$ROOT"/keys/extended.skey
+export POOL_ID=$(cat "$ROOT"/keys/pool/pool.id)
+
+# Wait for stake pool to start producing blocks
+sleep 20
+
+# Cleanup
+docker-compose down --volumes --remove-orphans
+
 #########
 # Vasil #
 #########
