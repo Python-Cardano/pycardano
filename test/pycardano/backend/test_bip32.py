2c2bbebd2a74a8362fb674acd610895195329915
Daehan Kim <daehan.koreastakepool@gmail.com>
2022-10-17 11:45:34 -0700

    ADD. porting over MAINNET address tests from Emurgo's cardano-serialization-lib test cases to increase MAINNET specific test coverage

--- test/pycardano/backend/test_bip32.py
+++ test/pycardano/backend/test_bip32.py
@@ -10,6 +10,8 @@ MNEMONIC_15 = "art forum devote street sure rather head chuckle guard poverty re
 MNEMONIC_24 = "excess behave track soul table wear ocean cash stay nature item turtle palm soccer lunch horror start stumble month panic right must lock dress"
 
 MNEMONIC_12_ENTROPY = "df9ed25ed146bf43336a5d7cf7395994"
+MNEMONIC_15_ENTROPY = "0ccb74f36b7da1649a8144675522d4d8097c6412"
+MNEMONIC_24_ENTROPY = "4e828f9a67ddcff0e6391ad4f26ddb7579f59ba14b6dd4baf63dcfdb9d2420da"
 
 
 def test_mnemonic():
@@ -33,6 +35,13 @@ def test_payment_address_12_reward():
             payment_part=None, staking_part=stake_vk.hash(), network=Network.TESTNET
         ).encode()
         == "stake_test1uqevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqp8n5xl"
+    )
+
+    assert (
+        Address(
+            payment_part=None, staking_part=stake_vk.hash(), network=Network.MAINNET
+        ).encode()
+        == "stake1uyevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqxdekzz"
     )
 
 
@@ -63,6 +72,13 @@ def test_payment_address_12_reward2():
         == "stake_test1uqevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqp8n5xl"
     )
 
+    assert (
+        Address(
+            payment_part=None, staking_part=stake_vk.hash(), network=Network.MAINNET
+        ).encode()
+        == "stake1uyevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqxdekzz"
+    )
+
 
 def test_payment_address_12_base():
     hdwallet = HDWallet.from_mnemonic(MNEMONIC_12)
@@ -79,6 +95,11 @@ def test_payment_address_12_base():
         == "addr_test1qz2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwq2ytjqp"
     )
 
+    assert (
+        Address(spend_vk.hash(), stake_vk.hash(), network=Network.MAINNET).encode()
+        == "addr1qx2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwqfjkjv7"
+    )
+
 
 def test_payment_address_15_base():
     hdwallet = HDWallet.from_mnemonic(MNEMONIC_15)
@@ -89,6 +110,11 @@ def test_payment_address_15_base():
     hdwallet_stake = hdwallet.derive_from_path("m/1852'/1815'/0'/2/0")
     stake_public_key = hdwallet_stake.public_key
     stake_vk = PaymentVerificationKey.from_primitive(stake_public_key)
+
+    assert (
+        Address(spend_vk.hash(), stake_vk.hash(), network=Network.TESTNET).encode()
+        == "addr_test1qpu5vlrf4xkxv2qpwngf6cjhtw542ayty80v8dyr49rf5ewvxwdrt70qlcpeeagscasafhffqsxy36t90ldv06wqrk2qum8x5w"
+    )
 
     assert (
         Address(spend_vk.hash(), stake_vk.hash(), network=Network.MAINNET).encode()
@@ -105,6 +131,11 @@ def test_payment_address_24_base():
     hdwallet_stake = hdwallet.derive_from_path("m/1852'/1815'/0'/2/0")
     stake_public_key = hdwallet_stake.public_key
     stake_vk = PaymentVerificationKey.from_primitive(stake_public_key)
+
+    assert (
+        Address(spend_vk.hash(), stake_vk.hash(), network=Network.TESTNET).encode()
+        == "addr_test1qqy6nhfyks7wdu3dudslys37v252w2nwhv0fw2nfawemmn8k8ttq8f3gag0h89aepvx3xf69g0l9pf80tqv7cve0l33sw96paj"
+    )
 
     assert (
         Address(spend_vk.hash(), stake_vk.hash(), network=Network.MAINNET).encode()
@@ -125,8 +156,20 @@ def test_payment_address_12_reward_from_entropy():
         == "stake_test1uqevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqp8n5xl"
     )
 
+    assert (
+        Address(
+            payment_part=None, staking_part=stake_vk.hash(), network=Network.MAINNET
+        ).encode()
+        == "stake1uyevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqxdekzz"
+    )
+
 
 def test_is_entropy():
-    entropy = "df9ed25ed146bf43336a5d7cf7395994"
-    is_entropy = HDWallet.is_entropy(entropy)
+    is_entropy = HDWallet.is_entropy(MNEMONIC_12_ENTROPY)
     assert is_entropy
+
+
+def test_is_entropy_wrong_input():
+    wrong_entropy = "df9ed25ed146bf43336a5d7cf73959"
+    is_entropy = HDWallet.is_entropy(wrong_entropy)
+    assert not is_entropy
